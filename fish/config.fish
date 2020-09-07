7dce785844c4c7c5d5975f746182fc1a2a2620f3
Thibault Théologien <thibault.theologien@wing.eu>
2020-09-07 12:35:51 +0200

    First configuration

---
+++ fish/config.fish
@@ -0,0 +1,7 @@
+# ENV
+set -U fish_user_paths /usr/local/bin $fish_user_paths
+set -g theme_color_scheme solarized-dark
+set -gx EDITOR "code -w"
+
+# ALIASES
+alias hstr="history --show-time='%h/%d - %H:%M:%S '"
---
+++ fish/functions/fish_prompt.fish
@@ -0,0 +1,1 @@
+/Users/thibaulttheologien/.local/share/omf/themes/bobthefish/fish_prompt.fish
